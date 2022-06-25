fb913e8e7707bc2b67381f64a77b51430401f1ae
Walhalen123 <107643948+Walhalen123@users.noreply.github.com>
2022-06-25 14:25:31 +0300

    Update Source.c

--- Source.c
+++ Source.c
@@ -1,63 +1,81 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
-struct tree
-{
-	struct tree** nodes;
+struct node_t {
+	struct node_t** children;
 	char* key;
-	int cnt;
+	unsigned int num_children;
+	int end_of_word;
 };
 
-
-struct tree* init_node(char* key)
+int is_it_node(struct node_t*child, char* key)
 {
-	struct tree* node = malloc(sizeof * node);
-	*(node->key) = *key;
-	node->nodes = NULL;
-	node->cnt = 0;
-	return node;
+	for (int i = 0; i < child->num_children; i++)
+	{
+		if (strcmp(child->children[i]->key, key) == 0)
+		{
+			return 0;
+		}
+	}
+	return 1;
 }
 
-struct tree* init_root()
+struct node_t* add_word_in_tree(struct node_t* root, char*word)
 {
-	struct tree* root = malloc(sizeof * root);
-	root->key = NULL;
-	root->cnt = 0;
-	root->nodes = NULL;
+	int index = 0;
+	int len = strlen(word);
+	char* ptr = malloc(1);
+	struct node_t* walker = root;
+	while (index < strlen(word))
+	{
+		ptr[index] = word[index];
+		ptr[index+1] = '\0';
+		if (is_it_node(walker, ptr) == 0)
+		{
+			for (int i = 0; i < walker->num_children; i++)
+			{
+				if (strcmp(walker->children[i]->key, ptr) == 0)
+				{
+					walker = walker->children[i];
+					break;
+				}
+			}
+		}
+		else
+		{
+			struct node_t* new_node = malloc(sizeof(new_node));
+			new_node->children = NULL;
+			if (index == len - 1)
+				new_node->end_of_word = 1;
+			else
+				new_node->end_of_word = 0;
+			new_node->key = malloc(sizeof(char*) * strlen(ptr));
+			strcpy(new_node->key, ptr);
+			
+			new_node->num_children = 0;
+			walker->num_children++;
+			walker->children = realloc(walker->children,sizeof(struct node_t*) * walker->num_children);
+			walker->children[walker->num_children - 1] = new_node;
+			printf("%s\n", walker->children[0]->key);
+			walker = walker->children[walker->num_children - 1];
+		}
+		index++;
+		ptr = realloc(ptr, sizeof(char) * (index + 1));
+	}
 	return root;
 }
 
-struct tree* add_new_word(struct tree* tree, char* word)
-{
-	int len = strlen(word);
-	int index = 0;
-	struct tree* walker = tree;
-	while (index < len)
-	{
-		for (int i = 0; i < walker->cnt; i++)
-		{
-			if (*word == *(walker->nodes[i]->key))
-			{
-				walker = walker->nodes[i]->key;
-				word++;
-				goto forward;
-			}
-		}
-		walker->cnt++;
-		walker->nodes = realloc(walker->nodes, walker->cnt);
-		walker->nodes[walker->cnt - 1] = init_node(*word);
-		walker = walker->nodes[walker->cnt - 1];
-		forward:
-		index++;
-	}
-
-	return tree;
-}
 
 int main()
 {
-	struct tree* tree = init_root();
-
-	return;
+	struct node_t* root = malloc(sizeof(root));
+	root->children = NULL;
+	root->end_of_word = 0;
+	root->key = NULL;
+	root->num_children = 0;
+	root = add_word_in_tree(root, "word");
+	root = add_word_in_tree(root, "words");
+	root = add_word_in_tree(root, "wordse");
+	printf("%s", root->children[0]->children[0]->key);
+	return 0;
 }
