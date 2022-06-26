4c6d5c9668a8ee2d36a51220b496a3ff8f0c0c41
grapeqna <101394346+grapeqna@users.noreply.github.com>
2022-06-26 12:15:46 +0300

    Add files via upload
    gotovo

--- scrabble.c
+++ scrabble.c
@@ -1,94 +1,135 @@
-﻿#include <stdio.h>
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <malloc.h>
+#include "yana.h"
 
 void print_optins()
 {
-	printf("1. Стартиране на нова игра\n");
-	printf("2. Настройки\n");
-	printf("	a. Променяне на броя на подаваните букви\n");
-	printf("	b. Променяне на броя рундове\n");
-	printf("3. Въвеждане на нова дума\n");
-	printf("4. Изход\n");
+	printf("1. Start new game\n");
+	printf("Settings\n");
+	printf("	2. Change the number of given letters\n");
+	printf("	3. Change the number of rounds\n");
+	printf("4. Add new word\n");
+	printf("5. Exit\n");
 }
 
 char* randomizing_letters(const int size)
 {
-	char* letters=mallco(size);
+	time_t t;
+	srand((unsigned)time(&t));
+
+	char* letters = malloc(size + 1);
 	char l;
 	for (int i = 0;i < size;i++)
 	{
-		l = (rand() % 26)+'A';
+		//l = (rand() % ('Z' - 'A' + 1)) + 'A';
+		l = 'A' + (rand() % 26);
 		letters[i] = l;
 	}
+	letters[size] = 0;
 	return letters;
 }
 
 
-int round(char*letters)
+int validating_word(char* letters, char* word)
 {
-	char* word = malloc(word);
-	int score;
-	printf("Букви: \n");
-	for (int i = 0;i < 10;i++)
+	int size_l = strlen(letters);
+	int size_w = strlen(word);
+	for (int i = 0;i < size_w;i++)
 	{
-		printf("%s, ", letters[i]);
+		for (int j = 0;j < size_l;j++)
+		{
+			if (word[i] == letters[j])
+				letters[j] = ' ';
+		}
 	}
-	scanf(word);
-	//proverka dali dumata e ot dadenite bukvi
-	//po 1 to4ka za izpolzvana bukva
-	//nakraq returnva score
+	int num = 0;
+	for (int b = 0;b < size_l;b++)
+	{
+		if (letters[b] == ' ')
+			num++;
+	}
+	if (num == size_w)
+		return 1;
+	else return 0;
+}
+
+int round(char* letters)
+{
+	int size = strlen(letters);
+	char* word = malloc(sizeof(char*));
+	int score = 0;
+	int num = 0;
+	printf("Letters: \n");
+	for (int i = 0;i < size;i++)
+	{
+		printf("%c ", letters[i]);
+	}
+	scanf("%s", word);
+	if (validating_word(letters, word) == 1)//trqbva da se proveri dali e v re4nika
+		score += strlen(word);
+
 	return score;
 }
 
 int main()
 {
 	print_optins();
-	char* letters = malloc(letters);
+	char* letters;
 	int option;
-	int score;
-	char op; char* word = malloc(word);
+	int score = 0;
+	int num = 0;
+	char* word = malloc(sizeof(word));
 
-	scanf(option);
+	scanf("%d", &option);
 	switch (option)
 	{
 	case 1://startirane
-		letters = randomizing_letters(10);
 		for (int i = 0;i < 10;i++)
 		{
+			letters = randomizing_letters(10);
 			score += round(letters);
+			printf("%d\n", score);
 		}
-		//vurtq 10 runda, rand bukvi, validirane duma dali e ot dadenite bukvi, validirane dali e v durvoto, 
-		
-	case 2:// nastroiki
-		printf("Какво искаш да смениш?\n");
-		scanf(op);
-		if (op == 'a')
-		{
-			printf("Колко букви искаш?\n");
-			int num;
-			scanf(num);
-			letters = randomizing_letters(num);
-			for (int i = 0;i < num;i++)
-			{
-				printf("%s, ", letters[i]);
-			}
-		}
-		else
-		{
-			printf("Колко рунда искаш?\n");
-			int round;
-			scanf(round);
-		}//zapo4va igra (basically copy/paste ot case1 ili vuv funkciq)
+		break;
 
-	case 3:// vuvezhdane na nova duma
-		
-		printf("Каква е думата?\n");
-		scnaf(word);
-		struct tree* tree = init_root();
-		add_new_word( tree, word);
-		printf("Думата бе добавена успешно\n");
+	case 2:// broi bukvi
+
+		printf("How many letters do you want?\n");
+		scanf("%d", &num);
+		for (size_t i = 0; i < 10; i++)
+		{
+			letters = randomizing_letters(num);
+			score += round(letters);
+			printf("%d\n", score);
+		}
+
+	case 3://broi rundove
+		printf("How many rounds do you want?\n");
+		int r = 0;
+		scanf("%d", &r);
+		//zapo4va igra (basically copy/paste ot case1 ili vuv funkciq)
+		for (int i = 0;i < r;i++)
+		{
+			letters = randomizing_letters(10);
+			score += round(letters);
+			printf("%d\n", score);
+		}
+		break;
+
+	case 4:// vuvezhdane na nova duma v re4nika
+		printf("What iS the word?\n");
+		scanf("%s", word);
+		//struct tree* tree = init_root();
+		//add_new_word( tree, word);
+		printf("Word was added\n");
+		break;
+
 	default://izhod
 		break;
+
 	}
 	return 0;
 }
