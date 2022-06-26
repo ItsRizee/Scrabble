4a74cdce6e6aaee1e77208255b5be7e2e34921f6
nadya <107633042+nadenica@users.noreply.github.com>
2022-06-26 11:10:25 +0300

    Add files via upload

---
+++ Nadya.c
@@ -0,0 +1,22 @@
+
+int validate_word(struct node_t* tree, char* word)
+{
+
+	char* part_of_word = calloc(sizeof * part_of_word * strlen(word)+1, 1);
+
+	for (int i = 0; i < strlen(word); i++)
+	{
+		int found_match = 0;
+		part_of_word[i] = word[i];
+		for(int j=0;j<tree->num_children;j++)
+			if (!strcmp(tree->children[j]->key, part_of_word))
+			{
+				tree= tree->children[j];
+				found_match++;
+					break;
+			}
+		if (!found_match)
+			return 0;
+	}
+	return tree->end_word;
+}
